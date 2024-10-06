43f40d25b95af6c6722bef129e0056ba645127ab
Gabriela Dourado <gdourado.066@gmail.com>
2024-10-05 23:53:10 -0300

    updating features

--- BlazorAppLinkShort/BlazorAppLinkShort.Client/obj/Debug/net8.0/BlazorAppLinkShort.Client.AssemblyInfo.cs
+++ BlazorAppLinkShort/BlazorAppLinkShort.Client/obj/Debug/net8.0/BlazorAppLinkShort.Client.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorAppLinkShort.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9551eef35737e536b0e59f24df903a8bf4505051")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee11caaeae593cc04babf3b4411cf94bebe18c00")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorAppLinkShort.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorAppLinkShort.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
